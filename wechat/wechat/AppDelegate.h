60666ecc777426bb8100835b3be4c3a454292bc1
LZF <LZF@163.com>
2016-03-12 11:40:07 +0800

    初始化项目purplewind

---
+++ wechat/wechat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wechat
+//
+//  Created by 梁梓烽 on 16/3/12.
+//  Copyright © 2016年 梁梓烽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wechat/wechat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wechat
+//
+//  Created by 梁梓烽 on 16/3/12.
+//  Copyright © 2016年 梁梓烽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
